2d4bd65f5987b1aa9d72962820cf8c1778c0d115
sreeharshakv <kvsharsha99@gmail.com>
2023-04-15 14:42:46 -0700

    frontend code and integration

--- src/main/java/com/semanticdataquery/Controller/RDFController.java
+++ src/main/java/com/semanticdataquery/Controller/RDFController.java
@@ -3,11 +3,13 @@ package com.semanticdataquery.Controller;
 import com.semanticdataquery.DTO.SelectQueryResponseDTO;
 import com.semanticdataquery.Util.Impl.RDFHelperImpl;
 import com.semanticdataquery.Util.RDFHelper;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class RDFController {
 
     RDFHelper rdfHelper = new RDFHelperImpl();
