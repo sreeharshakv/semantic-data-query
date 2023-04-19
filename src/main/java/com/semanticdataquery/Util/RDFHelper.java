c43a26477ec3b960e53473c9309ec77e6a23ee8c
sreeharshakv <kvsharsha99@gmail.com>
2023-04-19 02:07:38 -0700

    Exception handling and display error message on UI

--- src/main/java/com/semanticdataquery/Util/RDFHelper.java
+++ src/main/java/com/semanticdataquery/Util/RDFHelper.java
@@ -1,10 +1,14 @@
 package com.semanticdataquery.Util;
 
 import com.semanticdataquery.DTO.SelectQueryResponseDTO;
+import org.apache.jena.rdf.model.Model;
+import org.springframework.http.ResponseEntity;
 
 public interface RDFHelper {
 
-    SelectQueryResponseDTO processSelectQuery(String queryString);
+    ResponseEntity<SelectQueryResponseDTO> processSelectQuery(String queryString);
+
+    ResponseEntity<Boolean> processAskQuery(String queryString);
 
     Boolean processAskQuery(String queryString);
 
