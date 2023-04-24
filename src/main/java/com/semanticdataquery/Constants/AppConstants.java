f12989b9181c11ff2fdda5e677322c074eb4ded2
sreeharshakv <kvsharsha99@gmail.com>
2023-04-23 22:54:18 -0700

    Update datasets and load to model

--- src/main/java/com/semanticdataquery/Constants/AppConstants.java
+++ src/main/java/com/semanticdataquery/Constants/AppConstants.java
@@ -2,6 +2,7 @@ package com.semanticdataquery.Constants;
 
 public class AppConstants {
 
-    public static final String CRIME_RDF_FILE = "crime_2020_to_present.rdf";
+    public static final String CRIME_RDF_FILE_1 = "crime_2010_to_2019.rdf";
+    public static final String CRIME_RDF_FILE_2 = "crime_2020_to_present.rdf";
     public static final String HOUSING_OWL_FILE = "Housing.owl";
 }
