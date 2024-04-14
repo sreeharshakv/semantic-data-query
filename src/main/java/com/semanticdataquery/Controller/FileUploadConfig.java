ed7e96b83c65867ec8e6249b4b2413681409aa5f
sreeharshakv <kvsharsha99@gmail.com>
2024-04-14 04:56:23 -0700

    add file upload function

---
+++ src/main/java/com/semanticdataquery/Controller/FileUploadConfig.java
@@ -0,0 +1,20 @@
+package com.semanticdataquery.Controller;
+
+import jakarta.servlet.MultipartConfigElement;
+import org.springframework.boot.web.servlet.MultipartConfigFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.util.unit.DataSize;
+
+@Configuration
+public class FileUploadConfig {
+    @Bean
+    MultipartConfigElement multipartConfigElement() {
+        MultipartConfigFactory factory = new MultipartConfigFactory();
+        // Set max file size to 2MB
+        factory.setMaxFileSize(DataSize.parse("4MB"));
+        // Set max request size to 2MB
+        factory.setMaxRequestSize(DataSize.parse("4MB"));
+        return factory.createMultipartConfig();
+    }
+}
